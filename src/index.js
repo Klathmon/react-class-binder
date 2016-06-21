aa73d885917d1be0d55050da3b01022336b0ba28
Klathmon <gregbenner1@gmail.com>
2016-06-21 09:21:10 -0400

    revert back to 1.0.1 (broke something, and I don't have time now to figure out what)

--- src/index.js
+++ src/index.js
@@ -12,55 +12,29 @@ const methodsToIgnore = [
   'render'
 ]
 
-/**
- * Binds the given class's methods
- * @param  {Class} Super
- * @param  {Number} numberToLookBack The number of "steps" back from Component to bind methods on
- *                                   Don't use this unless you are me! It might go away at any time!
- * @return {Class}
- */
-export default function binder (Super, numberToLookBack = 3) {
+export default function binder (Super) {
   return class extends Super {
     constructor (...args) {
       super(...args)
+      let prototype = this
       // keep walking back the prototype chain until we find the "important" one
-      // The last element of the "prototypeChain" array is react's Constructor
-      const prototypeChain = getPrototypeChain(this)
-      // Get the prototype we care about by walking back a specific number from the end of the prototype chain I made
-      const prototypeToBind = prototypeChain[prototypeChain.length - numberToLookBack]
+      // The "important" one is the one right before this one, which is 2 before React's `Component`
+      while (true) {
+        prototype = Object.getPrototypeOf(prototype)
+        const parentPrototype = Object.getPrototypeOf(prototype)
+        const grandparentPrototype = Object.getPrototypeOf(parentPrototype)
+        if (parentPrototype instanceof Component && !(grandparentPrototype instanceof Component)) {
+          // At this point the prototype we want to work with is in the variable `prototype`
+          // So let's get it's properties, and set them all to bind on this!
+          for (let methodName of Object.getOwnPropertyNames(prototype)) {
+            // Don't bother binding any methods that don't need it...
+            if (!!~methodsToIgnore.indexOf(methodName)) continue // eslint-disable-line no-extra-boolean-cast
 
-      // Get all the property names for the prototype i care about
-      for (let methodName of Object.getOwnPropertyNames(prototypeToBind)) {
-        // Don't bother binding any methods that don't need it...
-        if (!!~methodsToIgnore.indexOf(methodName)) continue // eslint-disable-line no-extra-boolean-cast
-        // Ignore anything that's not a method
-        if (Object.getOwnPropertyDescriptor(prototypeToBind, methodName).value !== 'function') continue
-
-        this[methodName] = prototypeToBind[methodName].bind(this)
+            this[methodName] = prototype[methodName].bind(this)
+          }
+          break
+        }
       }
     }
   }
-}
-
-/**
- * Returns an array of "parent" prototypes walking up the chain. It ends at React's Component prototypeChain
- * @param  {Object} start `this` from the extending class
- * @return {Array}
- */
-function getPrototypeChain (start) {
-  const prototypeChain = [start]
-  do {
-    try {
-      prototypeChain.push(Object.getPrototypeOf(prototypeChain[prototypeChain.length - 1]))
-    } catch (e) {
-      break
-    }
-  } while (
-    prototypeChain < 3 || // While the prototypeChain is less than 3, just loop (we need at least 3 before doing any checking)
-    !( // If the following is false, keep going
-      prototypeChain[prototypeChain.length - 2] instanceof Component &&  // The parent must be an instanceof Component
-      !(prototypeChain[prototypeChain.length - 1] instanceof Component) // The grandparent must NOT be an instanceof Component
-    )
-  )
-  return prototypeChain
 }
--- src/react.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import binder from './index'
-
-React.BinderComponent = class BinderComponent extends binder(React.Component, 4) {}
-
-export default React
